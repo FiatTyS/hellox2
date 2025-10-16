143c78cfb7caf74af5047b97d5e26cb7366c8b49
FiatTyS <pacharasorn_k@cmu.ac.th>
2025-10-16 15:01:38 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Hello, World!</h1>
+      <h2>Deployed to GitHub Pages</h2>
+    </div>
+  );
+}
+
+export default App;
